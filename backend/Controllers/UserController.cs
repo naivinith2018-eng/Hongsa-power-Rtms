f3661330dc5314b30a2074ddb3b265c1c95101dc
Deang <naivinith2018@gmail.com>
2025-11-24 14:36:55 +0700

    first commit

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Hongsa.Rtms.Api.Models;
+
+namespace Hongsa.Rtms.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // api/User
+ public class UserController: ControllerBase
+
+{
+    [HttpGet]
+
+    public string GetUser()
+    {
+       return "Hello User" ;
+    }
+
+    [HttpGet ("WelcomeUser")]
+
+    public string WelcomeUser()
+    {
+       return "Welcome User" ;
+    }
+
+    [HttpPost ("PostUser")]
+
+    public string PostUser()
+    {
+       return "Post User" ;
+    }
+
+    [HttpPut ("PutUser")]
+
+    public string PutUser()
+    {
+       return "Put User" ;
+    }
+
+    [HttpDelete ("DeleteUser")]
+
+    public string DeleteUser()
+    {
+       return "Delete User" ;
+    }
+
+}
---
+++ backend/Program.cs
@@ -0,0 +1,33 @@
+using Scalar.AspNetCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+     // Scalar API Reference Configuration
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Hongsa RTMS API (Scalar)")
+            .WithTheme(ScalarTheme.Laserwave) // light, dark, purple
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
